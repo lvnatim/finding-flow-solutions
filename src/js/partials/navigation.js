d86028d19916872da724966a2b1ceeaf31bcb368
lvnatim <tim.lu@ourlog.ca>
2023-09-19 11:03:51 -0700

    feat: mobile navigation

--- src/js/partials/navigation.js
+++ src/js/partials/navigation.js
@@ -4,8 +4,29 @@ var $background     = $('#navigation_background');
 var $navigation     = $('#navigation');
 var $menuItems      = $('li.has-submenu');
 
+var toggle 	= document.querySelector('#toggle');
+var menu 	= document.querySelector('#menu');
+
+toggle.addEventListener('click', function(){
+    if (menu.classList.contains('is-active')) {
+      this.setAttribute('aria-expanded', 'false');
+      menu.classList.remove('is-active');
+    //   header.classList.remove('is-active');
+    } else {
+      menu.classList.add('is-active'); 
+    //   header.classList.add('is-active'); 
+      this.setAttribute('aria-expanded', 'true');
+    }
+  });
+
 $menuItems.each(function(index, el){
     el.querySelector('a').addEventListener("click",  function(event){
+        $menuItems.not(el).each(function(index, element){
+            var $this = $(this);
+            $this.removeClass('open');
+            $this.find('a').attr('aria-expanded', "false");
+        });
+
 		if (this.parentNode.className == "has-submenu") {
 			this.parentNode.className = "has-submenu open";
 			this.setAttribute('aria-expanded', "true");
@@ -13,6 +34,7 @@ $menuItems.each(function(index, el){
 			this.parentNode.className = "has-submenu";
 			this.setAttribute('aria-expanded', "false");
 		}
+
 		event.preventDefault();
         updateNavigation();
 		return false;
